f3357b68e0fc9fbb0094df0ad11357fdc80e8e32
mendoza91guada <mendoza91guada@gmail.com>
2026-04-15 00:12:50 -0300

    Busqueda por palabra clave

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -4,6 +4,7 @@
 #define N 5
 
 void MostrarPersonas(char * Lista[], int n);
+void BuscarNombre(char * Lista[], char palabra[]);
 
 int main(){
     char *Nombres[N];
@@ -21,6 +22,12 @@ int main(){
 
     MostrarPersonas(Nombres, N);
 
+    char palabra_b[30];
+    printf("\n============\nBUSQUEDA POR PALABRA");
+    printf("\nIngrese una palabra a buscar: ");
+    scanf("%s", palabra_b);
+
+    BuscarNombre(Nombres, palabra_b);
     return 0;
 }
 
@@ -28,6 +35,21 @@ void MostrarPersonas(char * Lista[], int n){
     printf("\nLos nombres ingresados son:\n");
     for (int i=0; i<n; i++){
         printf("%d - %s\n", i+1, Lista[i]);
+    }
+}
+
+void BuscarNombre(char * Lista[], char palabra[]){
+    int aux;
+    tolower(palabra);
+    for (int i=0; i<N; i++){
+        tolower(Lista[i]);
+        char *busqueda=strstr(Lista[i], palabra);
+        aux=i;
+    }
+    if (aux){
+        printf("\n%d- %s ",aux+1,Lista[aux]);
+    } else {
+        printf("\nNo se encontraron resultados. ");
     }
 }
 
