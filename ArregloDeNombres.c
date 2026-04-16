aa9455756194c3b51b74f360910fba5d13f30f8a
mendoza91guada <mendoza91guada@gmail.com>
2026-04-16 00:49:04 -0300

    Interfaz

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -10,7 +10,7 @@ void buscarNombrePorID(char *Lista[], int id);
 int main(){
     char *Nombres[N];
     char buffer[90];
-    int cant_letras;
+    int cant_letras, opcion;
 
     printf("\nIngrese %d nombres: \n", N);
     for (int i=0; i<N; i++){
@@ -23,22 +23,28 @@ int main(){
 
     MostrarPersonas(Nombres, N);
     
-    int id;
-    printf("\n====\nBúsqueda por ID");
-    printf("\nIngrese un número de 1 a %d: ", N);
-    scanf("%d", &id);
-   /* do{
-        printf("\nNúmero inválido.\nIngrese un número de 1 a %d: ", N);
+    printf("\n==================BÚSQUEDA DE RESULTADOS\n==================\n");
+    printf("\nUsted puede realizar una búsqueda del nombre por: \nOpción 1 - Búsqueda por ID: Ingrese un número de orden para hallar el nombre de esa posición.\nOpción 2 - Búsqueda por palabra clave: Ingrese una palabra para encontrar un nombre que la contenga.\n\n");
+    printf("Ingrese el número de la opción deseada: ");
+    scanf("%d", &opcion);
+
+    if (opcion==1){
+        int id;
+        printf("\n========================\nBúsqueda por ID\n");
+        printf("\nIngrese un número de 1 a %d: ", N);
         scanf("%d", &id);
-    } while (id>N);*/
-    buscarNombrePorID(Nombres, id);
-
-    char palabra_b[30];
-    printf("\n============\nBUSQUEDA POR PALABRA");
-    printf("\nIngrese una palabra a buscar: ");
-    scanf("%s", palabra_b);
-
-    BuscarNombrePorPalabra(Nombres, palabra_b);
+        buscarNombrePorID(Nombres, id);
+    } else {
+            if (opcion==2){
+                char palabra_b[30];
+                printf("\n========================\nBUSQUEDA POR PALABRA");
+                printf("\nIngrese una palabra a buscar: ");
+                scanf("%s", palabra_b);
+                BuscarNombrePorPalabra(Nombres, palabra_b);
+            } else {
+                printf("\nOpción inválida. Intente nuevamente");
+            }
+    }
 
     return 0;
 }
@@ -51,14 +57,20 @@ void MostrarPersonas(char * Lista[], int n){
 }
 
 void BuscarNombrePorPalabra(char * Lista[], char palabra[]){
-    int aux;
-    tolower(palabra);
+    int aux=-1;
+    int cant_palabra=strlen(palabra);
+    /*for (int i=0; i<cant_palabra; i++){
+        palabra[i]=tolower(palabra[i]);
+    }*/
     for (int i=0; i<N; i++){
-        tolower(Lista[i]);
+       /* Lista[i]=tolower(Lista[i]);*/
         char *busqueda=strstr(Lista[i], palabra);
+        if (busqueda!=NULL){
         aux=i;
+        }
     }
-    if (aux){
+    if (aux!=-1){
+        printf("\nRESULTADO: \n");
         printf("\n%d- %s ",aux+1,Lista[aux]);
     } else {
         printf("\nNo se encontraron resultados. ");
@@ -70,7 +82,8 @@ void buscarNombrePorID(char *Lista[], int id){
         printf("No se encontró el valor buscado.");
     } else {
         for (int i=0; i<N; i++){
-            if (i=id-1){
+            if (i==id-1){
+                printf("\nRESULTADO: \n");
                 printf("%d- %s", i+1, Lista[i]);
             }
         }
